17c9e36a43393c8ff825623a2cdcd27bfff75e0b
Jacob Pfeiffer <pfeiffer@hartford.edu>
2018-04-19 10:45:47 -0400

    added pug

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var mysql = require('mysql');
+var pug = require('pug');
 
-var app = express()
+var app = express();
+
+app.set('views', './view');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
-	res.send("alive");
+	res.render('index', {title: 'Hey', message: 'Hello there!', submessage: 'This is a subtitle'});
 });
 
 app.listen(8080, () => console.log('Listening on port 8080!'))
