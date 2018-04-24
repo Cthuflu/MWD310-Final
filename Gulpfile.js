c2204bdbc1d808df717c45f6eff86f098e67b6ea
Jon <jondreich@gmail.com>
2018-04-24 11:23:45 -0400

    yo i added sass also im stupid but there shouldnt be any merge conflicts lets gooo

---
+++ Gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass');
+
+gulp.task('styles', function(){
+	gulp.src('public/css/sass/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('default', function(){
+	gulp.watch('public/css/sass/*.scss', ['styles']);	
+})
