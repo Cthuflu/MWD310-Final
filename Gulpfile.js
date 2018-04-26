234bdfcffbcea58d8309803e608b19116adcfe68
Jacob Pfeiffer <pfeiffer@hartford.edu>
2018-04-26 11:17:11 -0400

    Server restarts automagically

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
+sass = require('gulp-sass'),
+spawn = require('child_process').spawn,
+node;
 
 gulp.task('styles', function(){
 	gulp.src('public/css/sass/*.scss')
@@ -8,5 +10,25 @@ gulp.task('styles', function(){
 });
 
 gulp.task('default', function(){
+	gulp.run('server')
+
+	gulp.watch(['./app.js'], function() {
+	    gulp.run('server')
+	})
+
 	gulp.watch('public/css/sass/*.scss', ['styles']);	
+})
+
+gulp.task('server', function() {
+  if (node) node.kill()
+  node = spawn('node', ['app.js'], {stdio: 'inherit'})
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+})
+
+process.on('exit', function() {
+    if (node) node.kill()
 })
